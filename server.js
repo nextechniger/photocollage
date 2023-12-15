606779a49d9c1d5be6aa8860adf4766c21ca5dbf
nextechniger <152731503+nextechniger@users.noreply.github.com>
2023-12-15 11:41:34 +0100

    Update

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-let downloadCount = 0;
+app.use(cookieParser());
 
-// Middleware pour autoriser les requêtes CORS (à ajuster selon tes besoins)
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.get('/', (req, res) => {
+    let visitCount = req.cookies.visitCount || 0;
+    visitCount = parseInt(visitCount) + 1;
+
+    res.cookie('visitCount', visitCount, { maxAge: 365 * 24 * 60 * 60 * 1000 }); // Cookie valide pendant 1 an
+
+    res.send(`
+        <h1>Bienvenue sur la Page</h1>
+        <p>Cette page a été visitée ${visitCount} fois.</p>
+    `);
 });
 
-// Endpoint pour obtenir le nombre de téléchargements
-app.get('/api/downloadCount', (req, res) => {
-  res.json({ count: downloadCount });
-});
-
-// Endpoint pour incrémenter le nombre de téléchargements
-app.post('/api/incrementDownload', (req, res) => {
-  downloadCount++;
-  res.json({ success: true });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
